ed7fb9ba3e2f9cb8b371bec5ab450b4d614e4619
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-09-06 16:33:32 +0300

    Clean up everything
    Signed-off-by: natalie-elizabeth <natalie.elizabeth@andela.com>

--- bin/cdk-work.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { CdkWorkStack } from '../lib/cdk-work-stack';
-
-const app = new cdk.App();
-new CdkWorkStack(app, 'CdkWorkStack');
--- lib/cdk-work-stack.ts
+++
@@ -1,7 +0,0 @@
-import cdk = require('@aws-cdk/core');
-export class CdkWorkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-  }
-}
